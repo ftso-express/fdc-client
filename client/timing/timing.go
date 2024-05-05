352229bee9ad136c5ccc76329609d9459f5813dd
Luka Avbreht <luka.avbreht@aflabs.si>
2024-05-05 10:17:40 +0200

    added timing

--- client/timing/timing.go
+++ client/timing/timing.go
@@ -39,3 +39,34 @@ func NextChoosePhaseEnd(t uint64) (*int, *uint64) {
 
 	return &roundID, &endTimestamp
 }
+
+type Round struct {
+	Start time.Time
+	ID    int
+}
+
+func GetRound(n int) *Round {
+	return &Round{time.Unix(t0, 0).Add(collectTime*time.Duration(n) - offset), n}
+
+}
+
+func GetRoundForTimestamp(t uint64) *Round {
+
+	round := int((t - t0 + 30) / 90)
+
+	return GetRound(round)
+
+}
+
+func (r *Round) Next() *Round {
+
+	return &Round{r.Start.Add(90 * time.Second), r.ID + 1}
+}
+
+func (r *Round) ToStart() time.Duration {
+	return time.Until(r.Start)
+}
+
+func RoundLatest() *Round {
+	return GetRoundForTimestamp(uint64(time.Now().Unix()))
+}
