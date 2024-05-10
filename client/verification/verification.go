d5b820fe9e7d71a1645d7648f3db108fe9c124a5
grePod <gregor.podlogar@aflabs.si>
2024-05-10 15:28:08 +0200

    verification fix

--- client/verification/verification.go
+++ client/verification/verification.go
@@ -13,6 +13,26 @@ type Request []byte
 
 type Response []byte
 
+// isStaticType checks whether bytes that represent abi.encoded request or response that encodes an instance of static type.
+// abi.encode(X) = enc((X)) of X of type T is encoding of tuple (X) of type (T). By specification, enc((X)) = head(X)tail(X).
+// If T is static, head(X) = enc(X) and tail(X) is empty. If T is dynamic, head(X) = bytes32(len(head(X))) = bytes32(32) and tail = enc(X).
+// See https://docs.soliditylang.org/en/latest/abi-spec.html for detailed specification.
+func isStaticType(bytes []byte) (bool, error) {
+
+	if len(bytes) < 96 {
+		return false, errors.New("bytes are to short")
+	}
+
+	first32Bytes := [32]byte(bytes[:32])
+
+	d := [32]byte{}
+
+	d[31] = byte(32)
+
+	return d != first32Bytes, nil
+
+}
+
 // AttestationType returns the attestation type of the request (the first 32 bytes).
 func (r Request) AttestationType() ([]byte, error) {
 
@@ -21,6 +41,7 @@ func (r Request) AttestationType() ([]byte, error) {
 	}
 
 	return r[:32], nil
+
 }
 
 // Source returns the source of the request (the second 32 bytes).
@@ -60,29 +81,82 @@ func (r Request) Mic() (common.Hash, error) {
 // ComputeMic computes Mic from the response.
 // Mic is the hash of the response with roundID set to 0.
 func (r Response) ComputeMic() (common.Hash, error) {
-	if len(r) < 128 {
-		return common.Hash{}, errors.New("response is to short")
+
+	static, err := isStaticType(r)
+
+	if err != nil {
+		return common.Hash{}, err
 	}
 
-	d := make([]byte, 32)
+	if static {
 
-	slices.Replace(d, 0, 32, r[64:96]...)
+		if len(r) < 128 {
+			return common.Hash{}, errors.New("response is to short")
+		}
 
-	zero32bytes := make([]byte, 32)
+		d := make([]byte, 32)
 
-	slices.Replace(r, 64, 96, zero32bytes...)
+		slices.Replace(d, 0, 32, r[64:96]...)
 
-	mic := crypto.Keccak256Hash(r)
+		zero32bytes := make([]byte, 32)
 
-	defer slices.Replace(r, 64, 96, d...)
+		slices.Replace(r, 64, 96, zero32bytes...)
 
-	return mic, nil
+		mic := crypto.Keccak256Hash(r)
+
+		defer slices.Replace(r, 64, 96, d...)
+
+		return mic, nil
+
+	} else {
+
+		if len(r) < 160 {
+			return common.Hash{}, errors.New("response is to short")
+		}
+
+		d := make([]byte, 32)
+
+		slices.Replace(d, 0, 32, r[96:128]...)
+
+		zero32bytes := make([]byte, 32)
+
+		slices.Replace(r, 96, 128, zero32bytes...)
+
+		mic := crypto.Keccak256Hash(r)
+
+		defer slices.Replace(r, 96, 128, d...)
+
+		return mic, nil
+
+	}
 
 }
 
 // AddRound sets the roundId in the response (third 32 bytes).
-func (r Response) AddRound(roundId uint64) ([]byte, error) {
-	if len(r) < 128 {
+func (r Response) AddRound(roundId uint64) (Response, error) {
+
+	static, err := isStaticType(r)
+
+	if err != nil {
+		return Response{}, err
+	}
+
+	if static {
+		if len(r) < 128 {
+			return []byte{}, errors.New("response is to short")
+		}
+
+		end := make([]byte, 32)
+		buf := make([]byte, 8)
+
+		binary.BigEndian.PutUint64(buf, roundId)
+
+		slices.Replace(end, 24, 32, buf...)
+
+		slices.Replace(r, 64, 96, end...)
+
+		return r, nil
+	} else if len(r) < 160 {
 		return []byte{}, errors.New("response is to short")
 	}
 
@@ -93,7 +167,7 @@ func (r Response) AddRound(roundId uint64) ([]byte, error) {
 
 	slices.Replace(end, 24, 32, buf...)
 
-	slices.Replace(r, 64, 96, end...)
+	slices.Replace(r, 96, 128, end...)
 
 	return r, nil
 
