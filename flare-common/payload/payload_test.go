981d955940b962be4a11ca8bc08c55c5c455273d
grePodFlr <gregor.podlogar@flare.network>
2024-06-04 10:33:14 +0200

    illegal characters in hex payload test

--- flare-common/payload/payload_test.go
+++ flare-common/payload/payload_test.go
@@ -104,6 +104,22 @@ var txError3 = &database.Transaction{
 	Timestamp:        1717417740,
 }
 
+var txError4 = &database.Transaction{
+	Hash:             "8dd67e88aa6f863aeb5cd62874530efd7dafef2d4a8cdf7fbf71844dab1c732z",
+	FunctionSig:      "6c532fae",
+	Input:            "6c532fae64000a003b002043a94d3c612d7f5cfd65e53a06d55bac77abbd2a6eb4dff766f51092db36ac664000a003b002043a94d3c612d7f5cfd65e53a06d55bac77abbd2a6eb4dff766f51092db36ac6",
+	BlockNumber:      16143116,
+	BlockHash:        "40888ee23c4d7da30c42f826ea187386eac4564b02ce801f0b0b91ef1e71da05",
+	TransactionIndex: 0,
+	FromAddress:      "6bba3b6fb0dc902845666fdad70b2a814a57b6f3",
+	ToAddress:        "2ca6571daa15ce734bbd0bf27d5c9d16787fc33f",
+	Status:           1,
+	Value:            "0",
+	GasPrice:         "37500000000",
+	Gas:              2500000,
+	Timestamp:        1717417740,
+}
+
 func TestExtractPayloadsError(t *testing.T) {
 
 	_, err := payload.ExtractPayloads(txError1)
@@ -124,6 +140,11 @@ func TestExtractPayloadsError(t *testing.T) {
 		t.Errorf("no error 3: %s", err)
 	}
 
+	_, err = payload.ExtractPayloads(txError4)
+
+	if err == nil {
+		t.Errorf("no error 4: %s", err)
+	}
 }
 
 var txMultiple = &database.Transaction{
