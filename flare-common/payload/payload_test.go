a0c5262ff9e11690550c6cc523005436ed197d98
Ryan Collingham <ryan.collingham@flare.network>
2024-06-24 11:42:01 +0100

    fix up payload test

--- flare-common/payload/payload_test.go
+++ flare-common/payload/payload_test.go
@@ -3,6 +3,7 @@ package payload_test
 import (
 	"flare-common/database"
 	"flare-common/payload"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -24,33 +25,64 @@ var tx = &database.Transaction{
 	Timestamp:        1717417740,
 }
 
+var txMultiple = &database.Transaction{
+	Hash:             "8dd67e88aa6f863aeb5cd62874530efd7dafef2d4a8cdf7fbf71844dab1c7327",
+	FunctionSig:      "6c532fae",
+	Input:            "6c532fae64000a003b002043a94d3c612d7f5cfd65e53a06d55bac77abbd2a6eb4dff766f51092db36ac6665000a003b002043a94d3c612d7f5cfd65e53a06d55bac77abbd2a6eb4dff766f51092db36ac66",
+	BlockNumber:      16143116,
+	BlockHash:        "40888ee23c4d7da30c42f826ea187386eac4564b02ce801f0b0b91ef1e71da05",
+	TransactionIndex: 0,
+	FromAddress:      "6bba3b6fb0dc902845666fdad70b2a814a57b6f3",
+	ToAddress:        "2ca6571daa15ce734bbd0bf27d5c9d16787fc33f",
+	Status:           1,
+	Value:            "0",
+	GasPrice:         "37500000000",
+	Gas:              2500000,
+	Timestamp:        1717417740,
+}
+
 func TestExtractPayloads(t *testing.T) {
 
-	payloadsOne, err := payload.ExtractPayloads(tx)
-
-	require.NoError(t, err)
-
-	if len(payloadsOne) != 1 {
-		t.Errorf("to many payloads")
+	tests := []struct {
+		tx           *database.Transaction
+		protocol     uint8
+		nuOfPayloads int
+		votingRound  uint64
+		length       uint16
+	}{
+		{
+			tx:           tx,
+			protocol:     100,
+			nuOfPayloads: 1,
+			votingRound:  655419,
+			length:       32,
+		},
+		{
+			tx:           txMultiple,
+			protocol:     101,
+			nuOfPayloads: 2,
+			votingRound:  655419,
+			length:       32,
+		},
 	}
 
-	payloadFTSO, ok := payloadsOne[100]
+	for i, test := range tests {
 
-	if !ok {
-		t.Error("no payload for FTSO")
-	}
+		payloads, err := payload.ExtractPayloads(test.tx)
 
-	if payloadFTSO.Protocol != 100 {
-		t.Error("mismatching protocol")
-	}
+		require.NoError(t, err, fmt.Sprintf("error in test %d", i))
 
-	if payloadFTSO.VotingRound != 655419 {
-		t.Errorf("wrong voting round %d", payloadFTSO.VotingRound)
+		require.Equal(t, test.nuOfPayloads, len(payloads), fmt.Sprintf("wrong number of payloads in test %d", i))
 
-	}
+		payloadFTSO, ok := payloads[test.protocol]
 
-	if payloadFTSO.Length != 32 {
-		t.Errorf("wrong  length %d", payloadFTSO.Length)
+		require.True(t, ok, fmt.Sprintf("missing payload in test %d", i))
+
+		require.Equal(t, test.protocol, payloadFTSO.Protocol, fmt.Sprintf("wrong protocol id in test %d", i))
+
+		require.Equal(t, test.votingRound, payloadFTSO.VotingRound, fmt.Sprintf("wrong voting round in test %d", i))
+
+		require.Equal(t, test.length, payloadFTSO.Length)
 
 	}
 
@@ -138,65 +170,18 @@ var txError5 = &database.Transaction{
 
 func TestExtractPayloadsError(t *testing.T) {
 
-	_, err := payload.ExtractPayloads(txError1)
-	require.Error(t, err)
-
-	_, err = payload.ExtractPayloads(txError2)
-	require.Error(t, err)
-
-	_, err = payload.ExtractPayloads(txError3)
-	require.Error(t, err)
-
-	_, err = payload.ExtractPayloads(txError4)
-	require.Error(t, err)
-
-	_, err = payload.ExtractPayloads(txError5)
-	require.Error(t, err)
-}
-
-var txMultiple = &database.Transaction{
-	Hash:             "8dd67e88aa6f863aeb5cd62874530efd7dafef2d4a8cdf7fbf71844dab1c7327",
-	FunctionSig:      "6c532fae",
-	Input:            "6c532fae64000a003b002043a94d3c612d7f5cfd65e53a06d55bac77abbd2a6eb4dff766f51092db36ac6665000a003b002043a94d3c612d7f5cfd65e53a06d55bac77abbd2a6eb4dff766f51092db36ac66",
-	BlockNumber:      16143116,
-	BlockHash:        "40888ee23c4d7da30c42f826ea187386eac4564b02ce801f0b0b91ef1e71da05",
-	TransactionIndex: 0,
-	FromAddress:      "6bba3b6fb0dc902845666fdad70b2a814a57b6f3",
-	ToAddress:        "2ca6571daa15ce734bbd0bf27d5c9d16787fc33f",
-	Status:           1,
-	Value:            "0",
-	GasPrice:         "37500000000",
-	Gas:              2500000,
-	Timestamp:        1717417740,
-}
-
-func TestExtractPayloadsMultiple(t *testing.T) {
-
-	payloadsMultiple, err := payload.ExtractPayloads(txMultiple)
-
-	require.NoError(t, err)
-	if len(payloadsMultiple) != 2 {
-		t.Errorf("to many payloads")
+	txs := []*database.Transaction{
+		txError1,
+		txError2,
+		txError3,
+		txError4,
+		txError5,
 	}
 
-	payloadFTSO, ok := payloadsMultiple[101]
+	for i, tx := range txs {
 
-	if !ok {
-		t.Error("no payload for FTSO")
-	}
-
-	if payloadFTSO.Protocol != 101 {
-		t.Error("mismatching protocol")
-	}
-
-	if payloadFTSO.VotingRound != 655419 {
-		t.Errorf("wrong voting round %d", payloadFTSO.VotingRound)
+		_, err := payload.ExtractPayloads(tx)
+		require.Error(t, err, fmt.Sprintf("error in test %d", i))
 
 	}
-
-	if payloadFTSO.Length != 32 {
-		t.Errorf("wrong  length %d", payloadFTSO.Length)
-
-	}
-
 }
